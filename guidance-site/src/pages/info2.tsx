f7de67bc36ecfc96e252d1661d35140f69fabeee
Andres Villalobos <andres.villalobos@datagrove.com>
2022-11-16 09:24:22 -0600

    added pages
    plan is to make a nav bar to move between pages

---
+++ guidance-site/src/pages/info2.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function contentPage1() {
+    return (
+    <h1>Here we'll have some content</h1>
+);
+}
+
+export default contentPage1;
---
+++ guidance-site/src/pages/info3.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function contentPage2() {
+    return (
+    <h1>Here we'll have some more content</h1>
+);
+}
+
+export default contentPage2;
