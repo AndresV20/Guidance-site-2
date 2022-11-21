5c6356936b72aa7aac5a322adc199de3ed114240
Andres Villalobos <andres.villalobos@datagrove.com>
2022-11-21 13:41:07 -0600

    added new pages routes

--- guidance-site/src/App.tsx
+++ guidance-site/src/App.tsx
@@ -2,20 +2,32 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LangCsharp from "./pages/Csharp";
+import LangGo from "./pages/Go";
+import LangHtmlCss from "./pages/Html-Css";
+import LangTS from "./pages/Typescript";
+import LangDart from "./pages/Dart";
 import NavBar from "./components/navBar";
+import FrameReact from "./pages/React";
+import FrameFlutter from "./pages/Flutter";
+import FrameCssTailwind from "./pages/TailwindCss";
 
 
 export interface AppProps{};
 const Application: React.FC<AppProps> = props => {
     return (
         <div>
-            {/* <div><NavBar  /></div> */}
             <BrowserRouter>
                 <NavBar  />
                 <Routes>
                     <Route path="/" element={<Home />}/>
                     <Route path = "/CSharp" element={<LangCsharp />}/>
-            
+                    <Route path = "/Go" element={<LangGo />}/>
+                    <Route path = "/Typescript" element={<LangTS />}/>
+                    <Route path = "/CSharp" element={<LangHtmlCss />}/>
+                    <Route path = "/Dart" element={<LangDart />}/>
+                    <Route path = "/React" element={<FrameReact />}/>
+                    <Route path = "/Flutter" element={<FrameFlutter />}/>\
+                    <Route path = "/TailwindCss" element={<FrameCssTailwind />}/>
                 </Routes>
             </BrowserRouter>
        </div>
