937869568d807f2f77f0dd6f002a1d7c88e623ae
Andres Villalobos <andres.villalobos@datagrove.com>
2022-11-21 10:46:11 -0600

    added nav bar and fixed white screen on surge

--- guidance-site/src/App.tsx
+++ guidance-site/src/App.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Language from "./pages/Languages";
+import NavBar from "./components/navBar";
+
 
 export interface AppProps{};
 const Application: React.FC<AppProps> = props => {
     return (
-       <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path = "languages" element={<Language />}/>
-        </Routes>
-       </BrowserRouter>
+        <div>
+            {/* <div><NavBar  /></div> */}
+            <BrowserRouter>
+                <NavBar  />
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path = "/languages" element={<Language />}/>
+            
+                </Routes>
+            </BrowserRouter>
+       </div>
     )
 };
 
